becddadfea7583cd3f9cbd4b76e66137dca29e72
Vesta-Nassone <vestanassone@gmail.com>
2021-04-24 13:55:32 +0200

    Implements the meals model.

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Meals(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    people = models.InterField()
+    price = models.DecimalField(max_digits=5)
+    preperation_time = models.IntegerField()
+    image = models.ImageField(upload_to='meals/')
