1a713e48886ad8f404753b5f00741caa809fab04
Vesta-Nassone <vestanassone@gmail.com>
2021-04-24 15:44:36 +0200

    Adds a meal context.

--- meals/views.py
+++ meals/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def meal_list(request):
     meal_list = Meals.objects.all()
 
+    context = {'meal_list': meal_list, }
+
     return render(request, 'meal/list.html', context)
+
 
 def meal_detail(request, slug):
     pass
