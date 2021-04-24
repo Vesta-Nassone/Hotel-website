d2493acd52bb1503e49ca165bc6e5ccfcffa21b4
Vesta-Nassone <vestanassone@gmail.com>
2021-04-24 15:38:38 +0200

    Creates a view form meals.

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def meal_list(request):
+    meal_list = Meals.objects.all()
+
+    return render(request, 'meal/list.html', context)
+
+def meal_detail(request, slug):
+    pass
