817ca6cef608aa154d11233dc376e377afe943fd
Vesta-Nassone <vestanassone@gmail.com>
2021-04-24 14:34:56 +0200

    Adds a slugField to meal model.

--- meals/models.py
+++ meals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Meals(models.Model):
@@ -8,6 +9,7 @@ class Meals(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     preperation_time = models.IntegerField()
     image = models.ImageField(upload_to='meals/')
+    slug = models.SlugField(blank=True, null=True)
 
 def __str__(self):
     return self.name
