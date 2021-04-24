7cf7a7aacaaa50fcc274bbc9773f40faf489dfdb
Vesta-Nassone <vestanassone@gmail.com>
2021-04-24 14:05:03 +0200

    Creates meals table and fields.

--- meals/models.py
+++ meals/models.py
@@ -5,6 +5,6 @@ class Meals(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     people = models.IntegerField()
-    price = models.DecimalField(max_digits=5)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
     preperation_time = models.IntegerField()
     image = models.ImageField(upload_to='meals/')
