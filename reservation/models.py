ff2214f99fe09cafcd16afb5c656c9d4da2007fa
Vesta-Nassone <vestanassone@gmail.com>
2021-04-28 15:38:26 +0200

    Refines the reservation models.

--- reservation/models.py
+++ reservation/models.py
@@ -5,9 +5,9 @@ class Reservation(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.IntegerField()
-    number_of_people = models.models.IntegerField()
-    date = models.models.DateField()
-    time = models.models.TimeField()
+    number_of_people = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
 
 
     def __str__(self):
