220904a972babd1e8b8ae860deb6ff0ab1c7af68
Vesta-Nassone <vestanassone@gmail.com>
2021-04-28 14:08:04 +0200

    Creates the reservation model.

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    number_of_people = models.models.IntegerField()
+    date = models.models.DateField()
+    time = models.models.TimeField()
+    
