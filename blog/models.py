0a1fd19e647184a1db0edf60674ad27268be27af
Vesta-Nassone <vestanassone@gmail.com>
2021-05-05 16:25:23 +0200

    Refactors the Post model.

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
