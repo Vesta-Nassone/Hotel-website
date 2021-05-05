416fafcf0e1bfa9152a5e79eb6387903f2d3e80b
Vesta-Nassone <vestanassone@gmail.com>
2021-05-05 15:22:47 +0200

    Adds form validation and saving methods.

--- reservation/views.py
+++ reservation/views.py
@@ -6,5 +6,11 @@ from .forms import ReserveTableForm
 
 
 def reserve_table(request):
-    context = {}
+    reserve_form = ReserveTableForm()
+    if request.method == 'POST':
+        reserve_form = ReserveTableForm(request.method)
+        if reserve_form.is_Valid():
+            reserve_form.save()
+
+    context = {'form': reserve_form}
     return render(request, 'Reservation/reservation.html', context)
