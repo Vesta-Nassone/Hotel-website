4d85f69f6399351387714b57dc786316e57d2e01
Vesta-Nassone <vestanassone@gmail.com>
2021-05-05 15:35:14 +0200

    Fixes the POST method for form validation.

--- reservation/views.py
+++ reservation/views.py
@@ -8,7 +8,7 @@ from .forms import ReserveTableForm
 def reserve_table(request):
     reserve_form = ReserveTableForm()
     if request.method == 'POST':
-        reserve_form = ReserveTableForm(request.method)
+        reserve_form = ReserveTableForm(request.POST)
         if reserve_form.is_valid():
             reserve_form.save()
 
