3be20e9e2ca87963b820a70749244df316a50b02
Vesta-Nassone <vestanassone@gmail.com>
2021-05-05 16:09:56 +0200

    Implements the posts class in blog models.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Posts(models.Model)
-    pass
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    # author
+    # tags
+    # category
+    created = models.DateTimeField(default=timezone.now)
+   
