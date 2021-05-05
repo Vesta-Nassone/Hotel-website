df8787b211a4e5fc8a854a3725fc6ce43e6562f4
Vesta-Nassone <vestanassone@gmail.com>
2021-05-05 15:32:21 +0200

    Fixes form validation method.

--- reservation/views.py
+++ reservation/views.py
@@ -9,7 +9,7 @@ def reserve_table(request):
     reserve_form = ReserveTableForm()
     if request.method == 'POST':
         reserve_form = ReserveTableForm(request.method)
-        if reserve_form.is_Valid():
+        if reserve_form.is_valid():
             reserve_form.save()
 
     context = {'form': reserve_form}
