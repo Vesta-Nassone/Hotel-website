33c173adf03088bc2de584eb91b2f50a8666f9b4
“Vesta <vestanassone@gmail.com>
2021-07-11 22:23:14 +0200

    Adds the property dictionary to Property models.

--- property/models.py
+++ property/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Property(models.Model):
+    property_type = {
+        {
+            'S',"sale",
+            'R', "rent"
+        }
+    }
+    name = models.CharField(max_length=50)
+    location = ''
