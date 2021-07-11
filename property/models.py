01d293608c157d16de94acfc637a5fb31d876dd0
“Vesta <vestanassone@gmail.com>
2021-07-11 22:37:12 +0200

    Removes the Property dictionary.

--- property/models.py
+++ property/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Property(models.Model):
-    property_type = {
-        {
-            'S',"sale",
-            'R', "rent"
-        }
-    }
+    # property_type = {
+    #     {
+    #         'S', 'sale',
+    #         'R', "rent"
+    #     }
+    # }
     name = models.CharField(max_length=50)
     location = ''
