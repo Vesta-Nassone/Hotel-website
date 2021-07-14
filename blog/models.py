acce688bb2e93fbeb6b8f208fdb045179c26de4d
“Vesta <vestanassone@gmail.com>
2021-07-14 19:16:04 +0200

    Implements the Comment Class section.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
-from .forms import forms
 
 # Create your models here.
 class Post(models.Model):
@@ -33,4 +32,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+class Comment(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    post = models.ForeignKey(Post , on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return str(self.content)
     
