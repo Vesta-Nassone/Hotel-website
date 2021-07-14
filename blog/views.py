8c158a236cef1212761675961ef39d40bb920f6b
“Vesta <vestanassone@gmail.com>
2021-07-14 14:06:26 +0200

    Fixes the post_list not showing error.

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def post_list(request):
     post_list = Post.objects.all()
 
     context = {
-        'posts_list': post_list,
+        'post_list': post_list,
     }
 
     return render(request, 'post/post_list.html', context)
