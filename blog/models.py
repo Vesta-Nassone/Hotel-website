ac18cf9b87f56f4986e661e15c73ba98e7b386c6
“Vesta <vestanassone@gmail.com>
2021-07-14 16:45:42 +0200

    Imports the form into models.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from .forms import forms
 
 # Create your models here.
 class Post(models.Model):
