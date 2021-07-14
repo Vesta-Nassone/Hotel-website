8ca5e1477cd3544091fa8eb300ea467d39bd8f6d
“Vesta <vestanassone@gmail.com>
2021-07-14 16:49:26 +0200

    Adds the NameForm context in post detail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # imports the posts from the database.
 from .models import Post, Category
+from .forms import NameForm
 
 # Create your views here.
 
@@ -16,9 +17,12 @@ def post_list(request):
 def post_detail(request, id):
     post_detail = Post.objects.get(id=id)
     categories = Category.objects.all()
+    # form = forms.URLField(label='URL')
+    form = NameForm()
     context ={
         'post_detail': post_detail,
-        'categories': categories
+        'categories': categories,
+        'form': form,
     }
 
     return render(request, 'post/post_detail.html', context)
