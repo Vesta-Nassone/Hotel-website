37650c3af39c79a31437f310ab57c2ecc749dc6c
“Vesta <vestanassone@gmail.com>
2021-07-14 19:15:41 +0200

    Redifines the forms file with CommentForm.

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from .models import Comment
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(max_length=100)
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = [
+            'content'
+        ]
