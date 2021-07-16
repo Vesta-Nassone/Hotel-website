a2fe2f6c02931f040b93993acf075e0e61edc764
“Vesta <vestanassone@gmail.com>
2021-07-16 17:27:29 +0200

    Implements the aboutus model.

--- aboutus/models.py
+++ aboutus/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class AboutUs(models.Model):
-    tittle = ""
-    content = ""
-    image = ""
+    tittle = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(upload_to='about_us/')
 
 class WhyChooseUs(models.Model):
     title = ""
     content = ""
 
+class Chefs(models.Model):
+    name = ""
+    subtitle = ""
+    image = ""
