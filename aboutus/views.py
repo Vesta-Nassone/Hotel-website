296d9d042bb764b4c53547985cbca34cd3ffb18b
“Vesta <vestanassone@gmail.com>
2021-07-16 19:10:51 +0200

    Implements the about us view.

--- aboutus/views.py
+++ aboutus/views.py
@@ -3,4 +3,15 @@ from .models import AboutUs, WhyChooseUs, Chefs
 
 # Create your views here.
 def aboutus_list(request):
-    pass
+    about = AboutUs.objects.last()
+    why_choose_us = WhyChooseUs.objects.last()
+    chef = Chefs.objects.last()
+
+    context = {
+        'about': about,
+        'why_choose_us': why_choose_us,
+        'chef': chef,
+
+    }
+
+    return render(request, 'aboutus/about.html', context)
