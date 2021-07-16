743c370e052e84edf77e6a47ddc8ae6c3f9120ac
“Vesta <vestanassone@gmail.com>
2021-07-16 17:44:13 +0200

    Defines a string repr for why choose us section.

--- aboutus/models.py
+++ aboutus/models.py
@@ -22,6 +22,10 @@ class WhyChooseUs(models.Model):
             verbose_name = _("why choose us")
             verbose_name_plural = _("why choose us")
 
+            def __str__(self):
+                return self.title
+            
+
 class Chefs(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
