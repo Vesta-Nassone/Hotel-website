99e9d16511e1698be24584a03de7b705ed237670
“Vesta <vestanassone@gmail.com>
2021-07-17 20:02:44 +0200

    Imports the Meals model.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from meals.models import Meals
 
 # Create your views here.
 def home(request):
+    meals = Meals.objects.all()
     context = {
+        'meals': meals
 
     }
 
