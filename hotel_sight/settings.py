5033c69f3179f758b51d6a19beadad6f256562a2
“Vesta <vestanassone@gmail.com>
2021-07-18 11:11:10 +0200

    Adds the Email backend service in django.

--- hotel_sight/settings.py
+++ hotel_sight/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
