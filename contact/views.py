6dd6c6bae6e03b7a759156fbe148e7fb974ff96b
“Vesta <vestanassone@gmail.com>
2021-07-18 11:35:01 +0200

    Implements the send email logic.

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from djanfo.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import ContactForm
 
 # Create your views here.
 def send_email(request):
-    pass
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
+
+    else:
+        form = ContactForm()
+
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'contact/contact.html', context)
 
 def send_success(request):
     pass
