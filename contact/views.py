2a1ada1b01a1e4da6b87311796009ba8cb829b8f
“Vesta <vestanassone@gmail.com>
2021-07-18 12:09:38 +0200

    Changes the form fields.

--- contact/views.py
+++ contact/views.py
@@ -8,12 +8,11 @@ def send_email(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            phone = form. cleaned_data['phone']
+            subject = form.cleaned_data['name']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(name, message, from_email, ['admin@example.com'])
+                send_mail(subject, message, from_email, ['admin@example.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid Header')
 
