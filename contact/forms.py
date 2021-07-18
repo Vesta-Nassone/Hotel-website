11208bac9ea7aa9f0f1df719078bacbce51234f3
“Vesta <vestanassone@gmail.com>
2021-07-18 12:07:36 +0200

    Removes the phone requirement from form.

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
-    phone = forms.CharField()
+    subject = forms.CharField()
+    #phone = forms.CharField()
     from_email = forms.EmailField(required=True)
     # message should behave like a Text area
     message = forms.CharField(widget=forms.Textarea, required=True)
