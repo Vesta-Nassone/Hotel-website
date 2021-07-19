62e3bc63ad8aa392c2f03e8c924c3324dace1926
“Vesta <vestanassone@gmail.com>
2021-07-19 23:09:59 +0200

    Adds the home posts view.

--- home/views.py
+++ home/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from meals.models import Meals, Category
+from blog.models import Post
 
 # Create your views here.
 def home(request):
     meals = Meals.objects.all()
     meals_list = Meals.objects.all()
     categories = Category.objects.all()
+    posts = Post.objects.all()
 
     context = {
         'meals': meals,
         'meals_list': meals_list,
         'categories': categories,
+        'posts': posts,
 
     }
 
